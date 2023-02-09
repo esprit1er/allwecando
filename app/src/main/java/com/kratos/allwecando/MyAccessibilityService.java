04c21115c3422949c10369c3871a08cff8603649
NLU2 <Noel.LODOU.Contractor@3ds.com>
2023-02-09 11:13:02 +0100

    log

--- app/src/main/java/com/kratos/allwecando/MyAccessibilityService.java
+++ app/src/main/java/com/kratos/allwecando/MyAccessibilityService.java
@@ -13,39 +13,29 @@ public class MyAccessibilityService  extends AccessibilityService {
 
     private static final String TAG = "MyAccessibilityService";
 
-
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         Log.e(TAG, "onAccessibilityEvent: "+AccessibilityEvent.obtain(event.getEventType()));
         AccessibilityNodeInfo rootInfo = getRootInActiveWindow();
-        dropClick(rootInfo);
-
-    }
-
-    public void dropClick(AccessibilityNodeInfo rootInfo){
-        for (AccessibilityNodeInfo node : rootInfo.findAccessibilityNodeInfosByText("BUY NOW"))
-        {
-            performdropClick(node.getParent());
+        for (AccessibilityNodeInfo node : rootInfo.findAccessibilityNodeInfosByText("BUY NOW")) {
+            performDropClick(node.getParent());
         }
     }
-    public void performdropClick(AccessibilityNodeInfo nodeParent){
-        if (nodeParent != null && nodeParent.isClickable()  ){
-            if (nodeParent.getChildCount()<3){
-                nodeParent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                try {
-                    Thread.sleep(239400);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
+    public void performDropClick(AccessibilityNodeInfo nodeParent) {
+        while (nodeParent != null && !nodeParent.isClickable()) {
+            nodeParent = nodeParent.getParent();
+        }
+        if (nodeParent != null && nodeParent.getChildCount() < 3) {
+            nodeParent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            try {
+                Thread.sleep(239400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }else if ((nodeParent != null) && (nodeParent.isClickable() == false)){
-            AccessibilityNodeInfo newnodeParent = nodeParent.getParent();
-            performdropClick(newnodeParent);
         }
     }
-
 
     @Override
     public void onInterrupt() {
@@ -55,13 +45,10 @@ public class MyAccessibilityService  extends AccessibilityService {
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
-
         info.notificationTimeout = 10;
-
         this.setServiceInfo(info);
         Log.d(TAG, "onServiceConnected: ");
     }
