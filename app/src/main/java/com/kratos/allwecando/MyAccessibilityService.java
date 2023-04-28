b4c255ce7fb0d90d545c70a4f6032d06f1070641
NLU2 <Noel.LODOU.Contractor@3ds.com>
2023-04-28 17:05:22 +0200

    update code

--- app/src/main/java/com/kratos/allwecando/MyAccessibilityService.java
+++ app/src/main/java/com/kratos/allwecando/MyAccessibilityService.java
@@ -1,40 +1,79 @@
 package com.kratos.allwecando;
-
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import androidx.annotation.RequiresApi;
 
-public class MyAccessibilityService  extends AccessibilityService {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyAccessibilityService extends AccessibilityService {
 
     private static final String TAG = "MyAccessibilityService";
+    private static final String BUY_NOW = "BUY NOW";
+    private AccessibilityNodeInfo mRootInfo;
+    private long mDelayTime = 239800; // 4 minutes
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private Map<String, AccessibilityNodeInfo> mCache = new HashMap<>();
+    private Map<String, AccessibilityNodeInfo> mClickedNodes = new HashMap<>();
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG, "onAccessibilityEvent: "+AccessibilityEvent.obtain(event.getEventType()));
-        AccessibilityNodeInfo rootInfo = getRootInActiveWindow();
-        for (AccessibilityNodeInfo node : rootInfo.findAccessibilityNodeInfosByText("BUY NOW")) {
-            performDropClick(node.getParent());
+        Log.e(TAG, "onAccessibilityEvent: " + AccessibilityEvent.obtain(event.getEventType()));
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
+                event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+
+            mRootInfo = getRootInActiveWindow();
+
+            if (mRootInfo != null) {
+                List<AccessibilityNodeInfo> nodes = mRootInfo.findAccessibilityNodeInfosByText(BUY_NOW);
+                for (AccessibilityNodeInfo node : nodes) {
+                    String key = node.toString();
+                    if (!mCache.containsKey(key) && !mClickedNodes.containsKey(key)) {
+                        mCache.put(key, node);
+                        performDropClick(node.getParent(), key);
+                    }
+                }
+            }
         }
     }
 
-    public void performDropClick(AccessibilityNodeInfo nodeParent) {
-        while (nodeParent != null && !nodeParent.isClickable()) {
-            nodeParent = nodeParent.getParent();
+    public void performDropClick(final AccessibilityNodeInfo nodeParent, final String key) {
+        if (nodeParent == null) {
+            return;
         }
-        if (nodeParent != null && nodeParent.getChildCount() < 3) {
-            nodeParent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            try {
-                Thread.sleep(239800);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        final AccessibilityNodeInfo clickableParent = findClickableParent(nodeParent);
+        if (clickableParent == null || clickableParent.getChildCount() >= 3) {
+            return;
+        }
+        clickableParent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        mClickedNodes.put(key, clickableParent);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                clickableParent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                mCache.remove(key);
+                mClickedNodes.remove(key);
+                clickableParent.recycle();
             }
+        }, mDelayTime);
+    }
+
+    private AccessibilityNodeInfo findClickableParent(AccessibilityNodeInfo node) {
+        while (node != null && !node.isClickable()) {
+            node = node.getParent();
         }
+        return node;
     }
 
     @Override
@@ -46,10 +85,19 @@ public class MyAccessibilityService  extends AccessibilityService {
     protected void onServiceConnected() {
         super.onServiceConnected();
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED | AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
         info.notificationTimeout = 10;
         this.setServiceInfo(info);
         Log.d(TAG, "onServiceConnected: ");
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mCache.clear();
+        mClickedNodes.clear();
+    }
+
 }
+
